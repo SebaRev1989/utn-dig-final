ecfccc7e7543d6dec91ae90ca3e3155392b1afc2
Sebastian Reverso <sheba1989@gmail.com>
2015-09-25 12:23:14 -0300

    Proyecto visual vacio

---
+++ gui/gestionSistemas.dpr
@@ -0,0 +1,14 @@
+program gestionSistemas;
+
+uses
+  Vcl.Forms,
+  formPrincipal in 'formPrincipal.pas' {frmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
